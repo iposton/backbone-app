d7b4baffb29f04c5ace9a3f86a7039799b5e3551
iposton <ianposton@sbcglobal.net>
2015-04-24 17:37:33 -0700

    css animations

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+
+
+ $('.ion-social-twitter').waypoint(function() {
+		$('.ion-social-twitter').addClass('animated fadeInUp')
+	}, {  
+				offset: '100%'
+
+	});
+
+
+
+
+});
